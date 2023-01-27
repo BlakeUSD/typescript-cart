42e6ed9118da4212518f8d9c38a6d15dd316b626
Dellano Blake <dellano.blake@outlook.com>
2023-01-27 00:09:45 -0500

    updated routes, made /store the default and added a message for users at blank routes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <h1>About</h1>
+      <span>Intentionally left blank. Actual project content is at the <Link to="/store">Store</Link> route</span>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <span>Intentionally left blank. Actual project content is at the <Link to="/store">Store</Link> route</span>
+    </div>
   )
 }
 
